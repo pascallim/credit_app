fd01560b58b0c021a6aa962f2892b50bc9473b83
Nathan Lorber <nlorber2211@gmail.com>
2022-08-04 18:17:58 +0200

    added port

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def calc_score(id: SK_ID):
 # 4. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run(app, host='https://credit-score-oc.herokuapp.com/')
+    uvicorn.run(app, host='https://credit-score-oc.herokuapp.com/', port=8000)
     #uvicorn.run(app, host='127.0.0.1', port=8000)
